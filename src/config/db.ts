ecb89f9d86112c04b4f8b4f34530ad72ab946e96
GerrardTech <FarrelGerrard17@gmail.com>
2026-06-11 09:57:33 +0700

    revised v2

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose';
 
-// ⚙️ Sesuaikan MONGODB_URI di file .env Anda
 const connectDB = async (): Promise<void> => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI as string);
